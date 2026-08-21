eade71a312fd7d665277df88d5035c8c13549c1a
VigneshRavisekar <vavz018@gmail.com>
2026-08-21 12:10:32 +0530

    just for unsigned overflow

--- tiny_soc/processor/test_full_adder.py
+++ tiny_soc/processor/test_full_adder.py
@@ -6,8 +6,18 @@ from cocotb.triggers import Timer
 async def basic_test(dut):
 
     for _ in range(5):
-        dut.a = random.randint(0,10)
-        dut.b = random.randint(0,10)
+        dut.a = random.randint(0,5)
+        dut.b = random.randint(0,5)
         await Timer(1,units="ns")
         print(f"A:{int(dut.a)},B:{int(dut.b)} ||| SUM {int(dut.sum)} CARRY_OUT {dut.cout}")
 
+@cocotb.test()
+async def overflow(dut):
+
+        dut.a = 14
+        dut.b = 15
+        await Timer(1,units="ns")
+        print(f"A:{int(dut.a)},B:{int(dut.b)} ||| SUM {int(dut.sum)} CARRY_OUT {dut.cout}")
+        if dut.cout == 1:
+             print("OVERFLOW DETECTED")
+            
