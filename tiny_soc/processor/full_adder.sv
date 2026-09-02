b5d4cea87821e212dc164b9417509629b93b3cea
VigneshRavisekar <vavz018@gmail.com>
2026-09-03 00:03:28 +0530

    Organised RTL and Verif

--- tiny_soc/processor/full_adder.sv
+++
@@ -1,9 +0,0 @@
-module full_adder(input logic a,b,cin, output logic sum,cout);
-
-        logic x;
-        xor_gate xor_1(.a(a),.b(b),.y(x));
-        xor_gate xor_2(.a(x),.b(cin),.y(sum));
-        assign cout = (x & cin) | (a & b);
-
-
-endmodule  
