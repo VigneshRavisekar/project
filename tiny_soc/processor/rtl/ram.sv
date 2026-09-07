8e91c714b55b656433bfd6fc1f5393315a40f503
VigneshRavisekar <vavz018@gmail.com>
2026-09-07 22:57:02 +0530

    bidirectional data support

--- tiny_soc/processor/rtl/ram.sv
+++ tiny_soc/processor/rtl/ram.sv
@@ -1,12 +1,12 @@
-module ram(input logic rst_n,clk,csn,rwn,input logic [3:0] addr, input logic [3:0] data_in, output logic [3:0] data_out);
+module ram(input logic rst_n,clk,csn,rwn,input logic [3:0] addr, inout logic [3:0] data);
 
                 logic [3:0] mem [0:15];
 
-                assign data_out = (!csn && rwn) ? mem[addr] : 4'bz;
+                assign data = (!csn && rwn) ? mem[addr] : 4'bz;
 
                 always @(posedge clk)begin
                     if (!rwn && !csn)
-                            mem[addr] <= data_in; 
+                            mem[addr] <= data; 
                             
                 end
 
