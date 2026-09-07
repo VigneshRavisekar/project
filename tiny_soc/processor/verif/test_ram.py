6c63a1aeb0357d91187d2dc24dcc44dbb735e0d9
VigneshRavisekar <vavz018@gmail.com>
2026-09-07 22:57:27 +0530

    included write test

--- tiny_soc/processor/verif/test_ram.py
+++ tiny_soc/processor/verif/test_ram.py
@@ -2,6 +2,8 @@ import cocotb
 import random
 from cocotb.clock import Clock
 from cocotb.triggers import Timer,RisingEdge,FallingEdge,ReadOnly
+from cocotb.handle import Force,Release
+from cocotb.types import LogicArray
 import logging
 
 log = logging.getLogger("cocotb")
@@ -16,16 +18,21 @@ async def test_write_read(dut):
             await Timer(3,"ns")
             dut.rst_n.value = 1
             dut.csn.value = 0
+            ## Memory Write Operation
             dut.rwn.value = 0
-            for addr in range(16):
-                    await RisingEdge(dut.clk)
-                    dut.addr.value = addr
-                    dut.data_in.value = random.randint(0,15)
-            await RisingEdge(dut.clk)
+            for address in range(16):
+                await RisingEdge(dut.clk)
+                dut.addr.value = address
+                dut.data.value = random.randint(0,15)
+                await Timer(0.5,"ns") 
+                log.info(f"Address: {dut.addr.value} WRITE_Data:{dut.data.value}")
+            dut.data.value = "ZZZZ"
+            await Timer(4,"ns")
             dut.rwn.value = 1
-            for addr in range(15,0,-1):
-                    await RisingEdge(dut.clk)
-                    dut.addr.value = addr
-                    await ReadOnly()
-                    log.info(dut.data_out.value)
+            for address in range(15,0,-1):
+                            await RisingEdge(dut.clk)
+                            dut.addr.value = address
+    
+
+          
             await Timer(100,"ns")
